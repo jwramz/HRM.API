bc8594843047922c37dee9047b4afb2d1ff1ccdb
Jose William Ingram Ramirez <jwingram@ineel.mx>
2023-05-23 15:52:56 -0600

    Agregar archivos de proyecto.

---
+++ Db/HRMContext.cs
@@ -0,0 +1,15 @@
+﻿using HRM.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRM.API.Db
+{
+    public class HRMContext : DbContext
+    {
+        public HRMContext(DbContextOptions<HRMContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace HRM.API.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; } = string.Empty;
+        public string? Designation { get; set; } = string.Empty;
+        public string? FathersName { get; set; } = string.Empty;
+        public string? MothersName { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+    }
+}
